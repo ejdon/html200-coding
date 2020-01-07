26742b141b372f5b9ee6dc4479671ec931b40a83
elena <ejdon@uw.edu>
2020-01-06 20:29:19 -0800

    adjusting exercise-three

--- lesson03/exercise-three.js
+++ lesson03/exercise-three.js
@@ -1,8 +1,6 @@
 let fruit = ['apple','orange','kiwi','banana','tomato'];
 
-let j = ','
-
-for (let i = 0; i < fruit.length; i++) {
-console.log(i + j + fruit[i]);
+for (let i = 1; i <= fruit.length; i++) {
+console.log(i + ',' + fruit[i - 1]);
 
 }
